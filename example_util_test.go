ecd96669e75592079588ea2503caca279eae27f6
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-04-03 13:57:10 +0200

    add more examples for utils

--- example_util_test.go
+++ example_util_test.go
@@ -7,12 +7,28 @@ import (
 	"github.com/studio-b12/whoops"
 )
 
+func ExampleUnwrapFull() {
+	type WrappedError struct {
+		whoops.InnerError
+	}
+
+	var err error
+	originErr := errors.New("some error")
+	err = whoops.WrapMessage(originErr, "Some message")
+	err = WrappedError{InnerError: whoops.InnerError{Inner: err}}
+
+	err = whoops.UnwrapFull(err)
+	fmt.Println(err == originErr)
+
+	// Output: true
+}
+
 func ExampleAs() {
 	type WrappedError struct {
 		whoops.InnerError
 	}
 
-	err := errors.New("Some error")
+	err := errors.New("some error")
 	err = whoops.WrapMessage(err, "Some message")
 	err = WrappedError{InnerError: whoops.InnerError{Inner: err}}
 
@@ -30,7 +46,7 @@ func ExampleIsOfType() {
 		whoops.InnerError
 	}
 
-	innerError := errors.New("Some error")
+	innerError := errors.New("some error")
 	var err error = whoops.WrapMessage(innerError, "Some message")
 	err = WrappedError{InnerError: whoops.InnerError{Inner: err}}
 
@@ -53,4 +69,68 @@ func ExampleFormat() {
 	err = whoops.WrapMessage(err, "Oh no!", "anyway")
 	msg = whoops.Format(err)
 	fmt.Println(msg)
+}
+
+func ExampleMessage() {
+	strErr := errors.New("some error")
+	dErr := whoops.WrapMessage(strErr, "some message")
+
+	fmt.Println(whoops.Message(strErr))
+	fmt.Println(whoops.Message(dErr))
+
+	// Output:
+	// some error
+	// some message
+}
+
+func ExampleJson() {
+	strErr := errors.New("some error")
+	dErr := whoops.WrapMessage(strErr, "some message", "some details")
+
+	json, _ := whoops.Json(strErr)
+	fmt.Println(json)
+
+	json, _ = whoops.Json(dErr)
+	fmt.Println(json)
+
+	// Details are excluded by default, but you can
+	// pass "true" for showDetails to include them
+	// in the JSON output.
+	json, _ = whoops.Json(dErr, true)
+	fmt.Println(json)
+
+	// Output:
+	// {
+	//   "error": "some error"
+	// }
+	// {
+	//   "error": "some error",
+	//   "message": "some message"
+	// }
+	// {
+	//   "error": "some error",
+	//   "message": "some message",
+	//   "details": "some details"
+	// }
+}
+
+func ExampleDetailsOfType() {
+	type status struct {
+		message string
+		code    int
+	}
+
+	err := errors.New("some error")
+	err = whoops.Wrap(err, "some string details")
+	err = whoops.Wrap(err, status{"some message", 42})
+
+	strDetails, _ := whoops.DetailsOfType[string](err)
+	fmt.Printf("strDetails: %s\n", strDetails)
+
+	statusDetails, _ := whoops.DetailsOfType[status](err)
+	fmt.Printf("statusDetails: %+v\n", statusDetails)
+
+	// Output:
+	// strDetails: some string details
+	// statusDetails: {message:some message code:42}
 }
